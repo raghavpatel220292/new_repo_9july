de41d11ad5cd01c597790cee965ee42ae15d0e46
raghavpatel220292 <raghav2202@zohomail.in>
2026-07-09 12:16:55 +0530

    chander added

--- provider.tf
+++ provider.tf
@@ -1,1 +1,12 @@
-terraform {}
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.80.0"
+      }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
