5775f5977e081bbcb51a465602af5ba4c742283b
raghavpatel220292 <raghav2202@zohomail.in>
2026-07-09 12:39:14 +0530

    stg account create

--- main.tf
+++ main.tf
@@ -7,3 +7,11 @@ resource "azurerm_resource_group" "rgtest" {
     name = "test3000"
     location = "westus"
 }
+
+resource "azurerm_storage_account" "stg"{
+  name = "stgjdjjfkj"
+  location = "eastus"
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+  resource_group_name = "test1"
+}
